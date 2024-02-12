26a1fa5b9716d131719bb4a96eb5bebc6ae6b5e8
Luchohak <a00958934@gmail.com>
2024-02-12 13:25:28 -0600

    Getting to know the basic types

---
+++ basics.ts
@@ -0,0 +1,26 @@
+//Primitives: number, string, boolean
+let age: number = 24;
+let userName: string = "Max";
+let isInstructor: boolean = true;
+
+//More complex types: arrays, objects
+let hobbies: string[];
+hobbies = ["Sports", "Coocking"];
+
+let person: {
+  name: string;
+  age: number;
+};
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+//An array of people
+let people: {
+  name: string;
+  age: number;
+}[];
+
+//Function types, parameters
